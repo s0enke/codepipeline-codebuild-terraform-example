57a67eba8bda98484c594223016e19173444c7e5
Soenke Ruempler <soenke@ruempler.eu>
2017-03-05 21:03:59 +0100

    Update us-west-1.tf

--- us-west-1.tf
+++ us-west-1.tf
@@ -3,7 +3,7 @@ provider "aws" {
     alias = "us-west-1"
 } 
 
-resource "aws_vpc" "sample" {
+resource "aws_vpc" "sample-us-west-1" {
     cidr_block = "10.34.0.0/16"
     provider = "aws.us-west-1"
 }
